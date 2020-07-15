0b6eb2c37ed3aae66357e3bb0c201cfc171ebcb1
Дроздовская Д.С <C:\Users\drozdovskaya\AppData\Roaming\The Bat! Pwd>
2020-07-15 17:26:28 +0300

    add new object after submitting the form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict'; // включаем строгий режим 
 
+const dataBase = [];
+
 const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
 const modalBtnSubmit = document.querySelector('.modal__btn-submit');
@@ -8,7 +10,7 @@ const catalog = document.querySelector('.catalog');
 const modalItem = document.querySelector('.modal__item');
 const keyCodeEsc = 27;
 const modalBtnWarning = document.querySelector('.modal__btn-warning');
-const elementsModalSubmit = [...modalSubmit.elements]               // ... - оператор спред spread - всё, что итерируется, он записывает через запятую. Спред как масло  размазваем.
+const elementsModalSubmit = [...modalSubmit.elements]               // ... - оператор спред spread - всё, что итерируется, он записывает через запятую. Спред как масло размазваем.
     .filter(elem => elem.tagName !== 'BUTTON'                       // выбираем все элементы, кроме тега button или типа submit 
                     && elem.type !== 'submit');                   
 
@@ -36,10 +38,25 @@ const closeModalEsc = event => {
 // * Событие - убираем надпись "Заполните все поля" и разблокируем кнопку отправки при внесения данных в поля input, textarea, select
 modalSubmit.addEventListener('input', () => {                           // событие input - отрабатывает при внесении изменений в value у тегов input, select или textarea
     const validForm = elementsModalSubmit.every(elem => elem.value);    // методом every проверяем все элементы в массиве. 
-                                                                        // Если все элементы вернут true - значение переменной будет true. Если хоть одно вернет false - будет false.  
-    modalBtnSubmit.disabled = !validForm;                               // Разблокируем кнопку отправки при значении !true (то есть false) переменной validForm
-    modalBtnWarning.style.display = validForm ? 'none' : '';            // Тернарный оператор. Если validForm true - присваиваем display = 'none';
+                                                                        // если все элементы вернут true - значение переменной будет true. Если хоть одно вернет false - будет false.  
+    modalBtnSubmit.disabled = !validForm;                               // разблокируем кнопку отправки при значении !true (то есть false) переменной validForm
+    modalBtnWarning.style.display = validForm ? 'none' : '';            // тернарный оператор. Если validForm true - присваиваем display = 'none';
 })
+
+// * Событие - отключаем обновление страницы, создаем новый объект в локальной БД после отправки формы Подачи объявления
+modalSubmit.addEventListener('submit', () => {  
+    event.preventDefault(); 
+    const itemObject = {};
+
+    for (const elem of elementsModalSubmit) {                           // цикл for of перебираем элементы формы, каждый раз создавая новую переменную (поэтому объявляется const)
+        itemObject[elem.name] = elem.value;                             // значение из name возьмется и сохранится как свойство объекта
+    } 
+    
+    dataBase.push(itemObject);                                          // добавляем новое объявление - объект - в массив dataBase
+    modalSubmit.reset();                                                // очищаем форму после отправки
+    
+    console.log(dataBase);
+});
 
 // * Событие отрытия модалки при клике на кнопку "Подать объявление"
 addAd.addEventListener('click', () => {                             // () => - callback функция, срабатываемая при клике
